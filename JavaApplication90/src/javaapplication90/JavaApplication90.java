63504b13e643d1fd9c026771bc7ed963742fc38f
bcutten <bcutten@ugcloud.ca>
2018-06-14 13:56:49 -0400

    added a comment to the header of main file

---
+++ JavaApplication90/src/javaapplication90/JavaApplication90.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package javaapplication90;
+
+/**
+ * This is an application which
+ * Created 14-Jun-2018 on 1:54:28 PM
+ * @author B. Cutten
+ */
+public class JavaApplication90 {
+
+   
+    public static void main(String[] args) {
+        // TODO cod application logic here
+    }
+
+}
